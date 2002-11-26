a31ca15ee3f1394dc7a1a5b4c7486b71879e7609
tomo <tomo>
2002-11-26 17:39:23 +0000

    New file.

---
+++ install-ids.el
@@ -0,0 +1,42 @@
+;;; install-ids.el --- installer of IDS files.
+
+;;; Code:
+
+(setq load-ids-simplify nil)
+
+(defun install-ids-read-file (file simplify soft)
+  (princ "Loading ")
+  (princ file)
+  (princ "...")
+  (ids-read-file file simplify soft)
+  (princ "done.\n"))
+
+(let ((i 1))
+  (while (<= i 12)
+    (install-ids-read-file (format "IDS-Daikanwa-%02d.txt" i)
+			   load-ids-simplify t)
+    (setq i (1+ i))))
+
+(install-ids-read-file "IDS-CBETA.txt" load-ids-simplify t)
+
+(install-ids-read-file "IDS-Daikanwa-dx.txt" load-ids-simplify t)
+
+(install-ids-read-file "IDS-Daikanwa-ho.txt" load-ids-simplify t)
+
+(install-ids-read-file "IDS-UCS-Basic.txt" load-ids-simplify t)
+
+(install-ids-read-file "IDS-UCS-Ext-A.txt" load-ids-simplify t)
+(let ((i 1))
+  (while (<= i 6)
+    (install-ids-read-file (format "IDS-UCS-Ext-B-%d.txt" i)
+			   load-ids-simplify t)
+    (setq i (1+ i))))
+(install-ids-read-file "IDS-JIS-X0208-1990.txt" load-ids-simplify t)
+
+(princ "Saving into ")
+(princ exec-directory)
+(princ "char-db/system-char-id/ideographic-structure...")
+(save-char-attribute-table 'ideographic-structure)
+(princ "done.\n")
+
+;;; install-ids.el ends hear
